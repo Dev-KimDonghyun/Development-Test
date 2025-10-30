95c25d0bc43501e27059513094df7957a7ea78eb
Dev-KimDonghyun <hyungus723@gmail.com>
2025-10-31 07:36:17 +0900

    feat: 출력문 연습

--- First/Program.cs
+++ First/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class WriteLineDemo
+{
+    static void Main()
+    {
+        Console.WriteLine("C# 출력문 연습입니다.");
+        Console.WriteLine("{0}, {0}", "Hello, C#");
+    }
+}
